42471be526acf6e9cb1c9c3a4e7b55b02febd360
Sylvain Galtier <sylvaingaltier@gmail.com>
2022-08-17 17:24:11 +0200

    garden banner with css and plant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaninng DB"
+
+Garden.destroy_all
+
+puts 'DB cleaned'
+
+puts "Creation of the first garden"
+
+Garden.create!(
+  name: "My Little Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+puts "first garden created"
+puts "Creation of the second garden"
+
+Garden.create!(
+  name: "My Other Garden",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+
+puts "second garden created"
